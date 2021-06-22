7594d96cef742841981e4db339d05a4d262e9c1e
Saloni Shedge <salonishedge09@gmail.com>
2021-06-23 00:49:21 +0530

    Fetching the news from API based on keyword

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css"></link>
+      <h1>News App</h1>
+      <form className="search" action=" ">
+        <input id="sal" type="text"></input>
+        <br/>
+        <button className="submit">Submit</button>
+      </form>
+
+      <div className="container">
+        <ul className="news-list"></ul>
+      </div>
+
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -15,3 +15,33 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const searchFrom=document.querySelector('.submit');
+const input=document.getElementById('sal');
+const newsList=document.querySelector('.news-list');
+
+searchFrom.addEventListener('click',retrieve)
+
+function retrieve(e){
+
+  e.preventDefault()
+  const apiKey='066029d94c0447cfaa68a4ba601b985c'
+  let topic=input.value;
+  let url=`https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`
+
+  fetch(url).then((res)=>{
+    return res.json()
+  }).then((data)=>{
+    console.log(data)
+    data.articles.forEach(article=>{
+      let li= document.createElement('li');
+      let a=document.createElement('a');
+      a.setAttribute('href',article.url);
+      a.setAttribute('target','_blank');
+      a.textContent=article.title;
+      li.appendChild(a);
+      newsList.appendChild(li);
+    })
+  })
+  console.log(topic);
+}
